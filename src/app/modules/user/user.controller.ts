7deef4172ede23b925201c2a7a31b6f5767af6c6
shakib-chahealth <mauijaloskor@gmail.com>
2025-11-16 10:37:16 +0600

    Response Formatting and Exception Handling

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,9 +2,21 @@ import { Request, Response } from "express";
 import { userService } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-  const result = await userService.createAdmin(req.body);
+  try {
+    const result = await userService.createAdmin(req.body);
 
-  res.send(result);
+    res.status(200).json({
+      success: true,
+      message: "Admin created successfully!",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message || "Something went wrong",
+      error: err,
+    });
+  }
 };
 
 export const userController = {
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -4,13 +4,24 @@ import bcrypt from "bcrypt";
 const prisma = new PrismaClient();
 
 const createAdmin = async (data: any) => {
-  const hashedPassword = await bcrypt.hash(data.password, 12);
+  const hashedPassword: string = await bcrypt.hash(data.password, 12);
 
   const userData = {
     email: data.admin.email,
     password: hashedPassword,
     role: UserRole.ADMIN,
   };
+
+  // check if user exist
+  const isUserExist = await prisma.user.findUnique({
+    where: {
+      email: userData.email,
+    },
+  });
+
+  if (isUserExist) {
+    throw new Error("User already exist...!");
+  }
 
   const result = await prisma.$transaction(async (transactionClient) => {
     await transactionClient.user.create({
